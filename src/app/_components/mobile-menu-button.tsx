3b68610c39fda032cfde131657d560444d00f59a
siobe34 <siobe14@hotmail.com>
2023-11-05 20:14:16 -0500

    button component for mobile menu

--- src/app/_components/mobile-menu-button.tsx
+++ src/app/_components/mobile-menu-button.tsx
@@ -1,36 +1,37 @@
-"use client";
-import { useEffect } from "react";
-import { usePathname } from "next/navigation";
-import { MenuIcon, XIcon } from "lucide-react";
+'use client';
+import { Button } from '@/app/_components/ui/button';
+import { cn } from '@/lib/utils';
+import { MenuIcon, XIcon } from 'lucide-react';
+import { usePathname } from 'next/navigation';
+import { useEffect } from 'react';
 
-import { Button } from "@/app/_components/ui/button";
-
-type Props = {
-  className: string;
+type MobileMenuButtonProps = {
+  className?: string;
 };
 
-export const MobileMenuButton = ({ className }: Props) => {
+export const MobileMenuButton = ({ className }: MobileMenuButtonProps) => {
   const route = usePathname();
 
   const toggleMobileMenu = () => {
-    const btnEl = document.getElementById("mobile-menu-button");
-    btnEl?.classList.toggle("active");
+    const btnEl = document.getElementById('mobile-menu-button');
+    btnEl?.classList.toggle('active');
   };
 
   useEffect(() => {
-    const btnEl = document.getElementById("mobile-menu-button");
-    btnEl?.classList.remove("active");
+    const btnEl = document.getElementById('mobile-menu-button');
+    btnEl?.classList.remove('active');
   }, [route]);
 
   return (
     <Button
-      id="mobile-menu-button"
-      className={`group ${className}`}
+      id='mobile-menu-button'
+      size='icon'
+      className={cn('group', className)}
       onClick={toggleMobileMenu}
     >
-      <MenuIcon className="rotate-0 scale-100 transition-all group-[.active]:-rotate-90 group-[.active]:scale-0" />
-      <XIcon className="absolute rotate-90 scale-0 transition-all group-[.active]:rotate-0 group-[.active]:scale-100" />
-      <span className="sr-only">Toggle menu for site links</span>
+      <MenuIcon className='rotate-0 scale-100 transition-all group-[.active]:-rotate-90 group-[.active]:scale-0' />
+      <XIcon className='absolute rotate-90 scale-0 transition-all group-[.active]:rotate-0 group-[.active]:scale-100' />
+      <span className='sr-only'>Toggle menu for site links</span>
     </Button>
   );
 };
