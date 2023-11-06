ba8ce9496cd6fcdedb5728b9dce691fdc0beae28
siobe34 <siobe14@hotmail.com>
2023-11-05 20:42:31 -0500

    columns definition for Tasks data table

---
+++ src/app/chores/_table/tasks-data-table-cols.tsx
@@ -0,0 +1,49 @@
+'use client';
+import { DataTableColumnHeader } from '@/app/_components/ui/data-table/data-table-column-header';
+import { readableStatusEnum } from '@/lib/mapStatusEnum';
+import { type TQueryTask } from '@/types/task';
+import { type ColumnDef } from '@tanstack/react-table';
+import { format } from 'date-fns';
+import { TasksDataTableActions } from './tasks-data-table-actions';
+
+export const TasksDataTableCols: ColumnDef<TQueryTask>[] = [
+  {
+    accessorKey: 'description',
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title='Description' />
+    ),
+  },
+  {
+    accessorKey: 'status',
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title='Status' />
+    ),
+    cell: ({ row }) => readableStatusEnum[row.original.status],
+    filterFn: (row, id, valueAsAny) => {
+      const value = valueAsAny as string[];
+      return value.includes(row.getValue(id));
+    },
+  },
+  {
+    accessorKey: 'assignedTo',
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title='User' />
+    ),
+    filterFn: (row, id, valueAsAny) => {
+      const value = valueAsAny as string[];
+      return value.includes(row.getValue(id));
+    },
+  },
+  {
+    accessorKey: 'completeBy',
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title='Complete By' />
+    ),
+    cell: ({ row }) => format(row.original.completeBy, 'PP'),
+  },
+  {
+    id: 'actions',
+    header: 'Actions',
+    cell: ({ row }) => <TasksDataTableActions rowData={row.original} />,
+  },
+];
