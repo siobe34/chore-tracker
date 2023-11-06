d105835eb6f9975da817d7879d0f012d0d217c0c
siobe34 <siobe14@hotmail.com>
2023-11-05 20:44:34 -0500

    adding Add Task component to page

--- src/app/chores/page.tsx
+++ src/app/chores/page.tsx
@@ -1,6 +1,7 @@
 import { api } from '@/trpc/server';
 import { TasksDataTable } from './_table/tasks-data-table';
 import { TasksDataTableCols } from './_table/tasks-data-table-cols';
+import { AddTask } from './add-task';
 
 export default async function Chores() {
   const users = await api.user.getAll.query();
@@ -8,6 +9,7 @@ export default async function Chores() {
 
   return (
     <main className='container flex h-full flex-col items-center gap-4 py-10'>
+      <AddTask className='w-full sm:w-auto sm:self-end' users={users} />
       <TasksDataTable columns={TasksDataTableCols} data={tasks} users={users} />
     </main>
   );
