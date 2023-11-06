b912264c65f6f38662afc20f1c0a9d251530f332
siobe34 <siobe14@hotmail.com>
2023-11-05 20:27:07 -0500

    add routers for User and Task models

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,6 @@
-import { postRouter } from "@/server/api/routers/post";
-import { createTRPCRouter } from "@/server/api/trpc";
+import { taskRouter } from '@/server/api/routers/task';
+import { userRouter } from '@/server/api/routers/user';
+import { createTRPCRouter } from '@/server/api/trpc';
 
 /**
  * This is the primary router for your server.
@@ -7,7 +8,8 @@ import { createTRPCRouter } from "@/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
+  user: userRouter,
+  task: taskRouter,
 });
 
 // export type definition of API
