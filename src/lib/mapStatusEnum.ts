bf3c974b1f4edace224ab3eec6ab4cbb19310234
siobe34 <siobe14@hotmail.com>
2023-11-05 20:30:22 -0500

    helpers to map Task "status" values to readable/original

---
+++ src/lib/mapStatusEnum.ts
@@ -0,0 +1,13 @@
+// * Map enum of "Task Status" to readable values
+export const readableStatusEnum = {
+  NOTSTARTED: 'Not Started',
+  INPROGRESS: 'In Progress',
+  COMPLETE: 'Complete',
+};
+
+// * Map readable enum values back to original
+export const enumFromReadable = {
+  'Not Started': 'NOTSTARTED',
+  'In Progress': 'INPROGRESS',
+  Complete: 'COMPLETE',
+};
