0bf764ae1d1c5f57a6ad8fc3bc61d286ea82db84
siobe34 <siobe14@hotmail.com>
2023-11-05 20:44:14 -0500

    adding Tasks data table to page

--- src/app/chores/page.tsx
+++ src/app/chores/page.tsx
@@ -1,4 +1,6 @@
 import { api } from '@/trpc/server';
+import { TasksDataTable } from './_table/tasks-data-table';
+import { TasksDataTableCols } from './_table/tasks-data-table-cols';
 
 export default async function Chores() {
   const users = await api.user.getAll.query();
@@ -6,6 +8,7 @@ export default async function Chores() {
 
   return (
     <main className='container flex h-full flex-col items-center gap-4 py-10'>
+      <TasksDataTable columns={TasksDataTableCols} data={tasks} users={users} />
     </main>
   );
 }
