0baf7dfc05c4fbc1854593cced9288ee1bd65395
siobe34 <siobe14@hotmail.com>
2023-11-05 20:10:29 -0500

    scroll to top component

--- src/app/_components/scroll-to-top.tsx
+++ src/app/_components/scroll-to-top.tsx
@@ -1,8 +1,7 @@
-"use client";
-import { useState, useEffect } from "react";
-import { ArrowUpCircleIcon } from "lucide-react";
-
-import { Button } from "@/app/_components/ui/button";
+'use client';
+import { Button } from '@/app/_components/ui/button';
+import { ArrowUpCircleIcon } from 'lucide-react';
+import { useEffect, useState } from 'react';
 
 export const ScrollToTop = () => {
   //* Intitialize whether or not to show the scroll to top button
@@ -10,19 +9,25 @@ export const ScrollToTop = () => {
 
   //* Add scroll event listener to determine the scroll position at the top of the page
   useEffect(() => {
-    window.addEventListener("scroll", () => {
+    const showOnScroll = () => {
       //* If the scroll position is anywhere except at the top of the page then show the scroll button
       if (window.scrollY > 0) setShowToTop(true);
       //* Otherwise the scroll position is at the top of the page so no need to show the scroll button
       else setShowToTop(false);
-    });
+    };
+
+    window.addEventListener('scroll', showOnScroll);
+
+    return () => {
+      window.removeEventListener('scroll', showOnScroll);
+    };
   }, []);
 
   //* Function to smoothly scroll to top of the page
   const smoothScrollToTop = () => {
     document.documentElement.scrollTo({
       top: 0,
-      behavior: "smooth",
+      behavior: 'smooth',
     });
   };
 
@@ -30,10 +35,11 @@ export const ScrollToTop = () => {
     <>
       {showToTop && (
         <Button
-          className="fixed bottom-8 right-8 z-30 inline-flex items-center justify-center bg-transparent text-foreground hover:bg-transparent"
+          className='fixed bottom-8 right-8 z-30 inline-flex items-center justify-center bg-transparent text-foreground hover:bg-transparent'
+          size='icon'
           onClick={() => smoothScrollToTop()}
         >
-          <ArrowUpCircleIcon className="hover:stroke-secondary" size={32} />
+          <ArrowUpCircleIcon size={32} />
         </Button>
       )}
     </>
