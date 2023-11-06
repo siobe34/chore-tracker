35a1275ae08b205fcc1f1244011f6eaae5b535d8
siobe34 <siobe14@hotmail.com>
2023-11-05 20:15:45 -0500

    theme toggler component with state from provider

--- src/app/_components/theme-toggler.tsx
+++ src/app/_components/theme-toggler.tsx
@@ -1,52 +1,52 @@
-"use client";
-import { useEffect } from "react";
-import { MoonIcon, SunIcon } from "lucide-react";
-
-import { Button } from "@/app/_components/ui/button";
+'use client';
+import { MoonIcon } from '@/app/_components/moon-icon';
+import { Button } from '@/app/_components/ui/button';
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
-} from "@/app/_components/ui/dropdown-menu";
+} from '@/app/_components/ui/dropdown-menu';
+import { MonitorSmartphone, SunIcon } from 'lucide-react';
+import { useTheme } from 'next-themes';
 
 export const ThemeToggler = () => {
-  useEffect(() => {
-    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-      setTheme("dark");
-    }
-  }, []);
+  const { setTheme } = useTheme();
 
-  const setTheme = (theme: "light" | "dark") => {
-    const appElement = document.getElementsByTagName("html")[0];
-    if (theme === "light") {
-      appElement?.classList.remove("dark");
-    }
-    if (theme === "dark") {
-      appElement?.classList.add("dark");
-    }
-  };
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button className="relative flex items-center justify-center">
-          <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all hover:stroke-secondary dark:-rotate-90 dark:scale-0" />
-          <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all hover:stroke-secondary dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
+        <Button
+          size='icon'
+          className='relative flex items-center justify-center'
+        >
+          <SunIcon className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all hover:stroke-secondary dark:-rotate-90 dark:scale-0' />
+          <MoonIcon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all hover:stroke-secondary dark:rotate-0 dark:scale-100' />
+          <span className='sr-only'>Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent>
         <DropdownMenuItem
-          className="cursor-pointer"
-          onClick={() => setTheme("light")}
+          className='cursor-pointer'
+          onClick={() => setTheme('light')}
         >
+          <SunIcon className='pr-2' />
           Light
         </DropdownMenuItem>
         <DropdownMenuItem
-          className="cursor-pointer"
-          onClick={() => setTheme("dark")}
+          className='cursor-pointer'
+          onClick={() => setTheme('dark')}
         >
+          <MoonIcon className='h-6 w-6 pr-2' />
           Dark
+        </DropdownMenuItem>
+
+        <DropdownMenuItem
+          className='cursor-pointer'
+          onClick={() => setTheme('system')}
+        >
+          <MonitorSmartphone className='pr-2' />
+          System
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
