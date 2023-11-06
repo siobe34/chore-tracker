2879191b8134f2d8cd86d4c9a29a48ffd323cede
siobe34 <siobe14@hotmail.com>
2023-11-05 20:50:31 -0500

    adding Add User component to page

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,12 +1,14 @@
 import { api } from '@/trpc/server';
 import { UsersDataTable } from './_table/users-data-table';
 import { UsersDataTableCols } from './_table/users-data-table-cols';
+import { AddUser } from './add-user';
 
 export default async function Users() {
   const users = await api.user.getAll.query();
 
   return (
     <main className='container flex h-full flex-col items-center gap-4 py-10'>
+      <AddUser className='w-full sm:w-auto sm:self-end' />
       <UsersDataTable data={users} columns={UsersDataTableCols} />
     </main>
   );
