a70e718852cf7edae901de354c4ba9a867c12b95
siobe34 <siobe14@hotmail.com>
2023-11-05 20:43:45 -0500

    fetching Users and Tasks

--- src/app/chores/page.tsx
+++ src/app/chores/page.tsx
@@ -1,4 +1,9 @@
+import { api } from '@/trpc/server';
+
 export default async function Chores() {
+  const users = await api.user.getAll.query();
+  const tasks = await api.task.getAll.query();
+
   return (
     <main className='container flex h-full flex-col items-center gap-4 py-10'>
     </main>
