290420f83394d4e83a13675c6c2e839696643bae
siobe34 <siobe14@hotmail.com>
2023-11-07 00:27:10 -0500

    page displaying completed Tasks

---
+++ src/app/chores/complete/page.tsx
@@ -0,0 +1,16 @@
+import { api } from '@/trpc/server';
+import { TasksDataTable } from '../_table/tasks-data-table';
+import { TasksDataTableCols } from '../_table/tasks-data-table-cols';
+
+export default async function Chores() {
+  const users = await api.user.getAll.query();
+  const tasks = await api.task.getByStatus.query({
+    status: ['COMPLETE'],
+  });
+
+  return (
+    <main className='container flex h-full flex-col items-center gap-4 py-10'>
+      <TasksDataTable columns={TasksDataTableCols} data={tasks} users={users} />
+    </main>
+  );
+}
